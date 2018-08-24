6c95280b8cc030d2168b4d1b5e08088b4194dead
stigmas <melocotonero00@gmail.com>
2018-08-25 00:25:04 +0200

    Starting project

---
+++ Domain/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Common.Models.Product> Products { get; set; }
+    }
+}
---
+++ SalesBackend/Models/LocalDataContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalesBackend.Models
+{
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<Common.Models.Product> Products { get; set; }
+    }
+}
